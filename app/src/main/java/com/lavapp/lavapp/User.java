17256f8422c5fb7ab1b5883509793006e082c055
alang <alan.snchz0@gmail.com>
2017-10-31 00:12:41 -0700

    first commit :) ;

---
+++ app/src/main/java/com/lavapp/lavapp/User.java
@@ -0,0 +1,102 @@
+package com.lavapp.lavapp;
+
+/**
+ * Created by alang on 10/13/2017.
+ */
+
+public class User {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String birthday;
+    private String gender;
+    private String password;
+    private String createdAt;
+    private String updatedAt;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String email, String birthday, String gender, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
